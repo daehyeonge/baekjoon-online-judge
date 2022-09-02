dee4cf81556314cf29183523c7d02f099cf274be
eoguddl <nicenicnic123@gmail.com>
2022-09-02 23:53:27 +0900

    1874 - 스택 수열

---
+++ 1929 - 소수 구하기.py
@@ -0,0 +1,14 @@
+def solution(num):
+    if num == 1:
+        return False
+    else:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        print(num)
+
+
+M, N = map(int, input().split())
+
+for i in range(M, N + 1):
+    solution(i)
